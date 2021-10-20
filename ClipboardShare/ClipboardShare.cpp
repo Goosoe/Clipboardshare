85e467f7053f6a50b3ba672305d73a1bbb314af0
Goosoe <miguel.al.lourenco@gmail.com>
2021-10-20 09:45:25 +0100

    initial commit

---
+++ ClipboardShare/ClipboardShare.cpp
@@ -0,0 +1,12 @@
+﻿// ClipboardShare.cpp : Defines the entry point for the application.
+//
+
+#include "ClipboardShare.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
